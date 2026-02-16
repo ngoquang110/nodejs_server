7baa46d4357f1878a761142dcd1798a03018c1b2
ngoquang110 <148013723+ngoquang110@users.noreply.github.com>
2026-02-16 13:15:37 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -17,6 +17,8 @@ logger.info('Logging logging all the way');
 logger.info('Something');
 logger.info('New line in remote and local');
 logger.info('Test in remote');
+logger.info('Test in remote 2');
+
 
 
 
